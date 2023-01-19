781435b93e5a51095c6663743b05213e5958065c
Kevin Paul <kpjtofficial@gmail.com>
2023-01-19 23:24:39 +0530

    Updated tailwind.config

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -1,16 +1,8 @@
-"use client"
 import { NextPage } from 'next'
-import Head from 'next/head'
 
 const Contribute: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>OneRoof | Contribute</title>
-        <meta name="description" content="Property details page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className='min-h-screen flex items-center justify-center'>
         Contribute
       </main>
