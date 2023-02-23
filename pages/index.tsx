775f90374aa484b2962b76c3b060f53589b1d587
itaintkevin <kpjtofficial@gmail.com>
2023-02-23 21:22:22 +0530

    fix: updated description

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home = ({ properties }: Props) => {
     <>
       <Head>
         <title>OneRoof</title>
-        <meta name="description" content="Renting options near SRM made easy." />
+        <meta name="description" content="Finding your next home made easy!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
